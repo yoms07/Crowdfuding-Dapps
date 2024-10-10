8bf979f98a983d30a3397927d38992778906a7dc
Jason Stanley <jasonstanleyyoman@gmail.com>
2024-10-10 11:57:26 +0700

    tidying up config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,7 @@ import { HardhatUserConfig, task } from "hardhat/config";
 import { v4 } from "uuid";
 import "@nomicfoundation/hardhat-toolbox";
 
-// import DeployedAddress from "./ignition/deployments/chain-31337/deployed_addresses.json";
+import DeployedAddress from "./ignition/deployments/chain-31337/deployed_addresses.json";
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
@@ -27,7 +27,7 @@ task("topUp", "TopUp Balance")
 
     const contract = await hre.ethers.getContractAt(
       "KBToken",
-      // DeployedAddress["KBTokenModule#KBToken"],
+      DeployedAddress["KBTokenModule#KBToken"],
       adminSigner
     );
     const numberAmount = parseInt(taskArgs.amount);
@@ -41,7 +41,7 @@ task("kbbalance")
     const signer = await hre.ethers.provider.getSigner(0);
     const contract = await hre.ethers.getContractAt(
       "KBToken",
-      // DeployedAddress["KBTokenModule#KBToken"],
+      DeployedAddress["KBTokenModule#KBToken"],
       signer
     );
 
@@ -67,7 +67,7 @@ task("cflist").setAction(async (taskArgs, hre) => {
   const signer = await hre.ethers.provider.getSigner(0);
   const contract = await hre.ethers.getContractAt(
     "KBFactory",
-    // DeployedAddress["KBFactoryModule#KBFactory"],
+    DeployedAddress["KBFactoryModule#KBFactory"],
     signer
   );
 
@@ -75,59 +75,6 @@ task("cflist").setAction(async (taskArgs, hre) => {
   cfList.forEach((cf) => {
     console.log(cf);
   });
-});
-
-task("inspectcf")
-  .addParam("address", "Crowdfunding Address")
-  .setAction(async (taskArgs, hre) => {
-    const signer = await hre.ethers.provider.getSigner(0);
-    const contract = await hre.ethers.getContractAt(
-      "Crowdfunding",
-      taskArgs.address,
-      signer
-    );
-
-    const target = await contract.getTarget();
-    const current = await contract.current();
-    const deadline = await contract.deadline();
-    const isOpen = await contract.isOpen();
-    const metadataCID = await contract.metadataCID();
-
-    console.log("Target: ", target);
-    console.log("Current: ", current);
-    console.log("Deadline: ", deadline);
-    console.log("IsOpen: ", isOpen);
-  });
-
-// task("createcf").setAction(async (taskArgs, hre) => {
-//   const signer = await hre.ethers.provider.getSigner(19);
-//   const contract = await hre.ethers.getContractAt(
-//     "KBFactory",
-//     DeployedAddress["KBFactoryModule#KBFactory"],
-//     signer
-//   );
-
-//   const cfMetadata = {
-//     title: "Title",
-//     categories: ["Sport", "Technology"],
-//     description: "Description",
-//     random: v4(),
-//   };
-
-//   const { cid } = await createIPFSClient().add(JSON.stringify(cfMetadata));
-
-//   await contract.createCrowdfunding(cid.toString(), 100_000_000, 1734627600);
-// });
-
-task("seed").setAction(async (taskArgs, hre) => {
-  const account = await hre.ethers.provider.getSigner(19);
-  await hre.run("topUp", {
-    account: account.address,
-    amount: "200000000",
-  });
-  await hre.run("createcf");
-  console.log("New created crowdfunding address: ");
-  await hre.run("cflist");
 });
 
 const config: HardhatUserConfig = {
